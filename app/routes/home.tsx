265af2419a17b4a050abaa1a9a64215064ec25a8
EmanuelIna <emanuelinacio052@gmail.com>
2026-07-19 21:28:42 -0400

    mock data e prompts para criados
    props e dados simulados criados no arquivo index.ts no diretorio constants

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,24 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Roudter!" },
+    { title: "Currats" },
+    { name: "description", content: "Análise modo ATS para seu currículo" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Verifique a nota do seu currículo</h1>
+        <h2>Seu currículo avaliado por IA para cada vaga</h2>
+      </div>
+    </section>
+
+
+  </main>
 }
